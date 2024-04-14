5fd3332503c2dd9a5024851299566f6790dce6cc
Shimaa-Mahmoud-Abdelazeem <142457787+Shimaa-Mahmoud-Abdelazeem@users.noreply.github.com>
2024-04-14 04:56:09 +0200

    add contact-us.html and map.js

---
+++ src/assets/js/map.js
@@ -0,0 +1,25 @@
+// Initialize the map centered on Cairo, Egypt
+var map = L.map("map").setView([30.0444, 31.2357], 13);
+
+// Add a tile layer
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 25,
+}).addTo(map);
+
+
+var marker = L.marker([30.0444, 31.2357]).addTo(map);
+
+
+// Define a custom icon with the desired color
+// var customIcon = L.icon({
+//     iconUrl: 'location-icon.svg', // Replace with the path to your SVG icon
+//     iconSize: [80, 80], // Set the icon size
+// });
+
+// Add a marker to Cairo, Egypt with the custom icon
+// var marker = L.marker([30.0444, 31.2357], { icon: customIcon }).addTo(map);
+
+// Add a popup to the marker
+marker.bindPopup("<b>Welcome to Cairo, Egypt!</b><br>This is a beautiful city.").openPopup();
+
+

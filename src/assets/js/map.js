5ba848865abbbfc2de428f09ef03ec1bce56dd4b
Shimaa-Mahmoud-Abdelazeem <142457787+Shimaa-Mahmoud-Abdelazeem@users.noreply.github.com>
2024-04-16 01:37:01 +0200

    added contact-us2.html and modify map.js

--- src/assets/js/map.js
+++ src/assets/js/map.js
@@ -7,19 +7,22 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 
-var marker = L.marker([30.0444, 31.2357]).addTo(map);
+// var marker = L.marker([30.0444, 31.2357]).addTo(map);
 
 
-// //Define a custom icon with the desired color
-// var customIcon = L.icon({
-//     iconUrl: '../images/location-icon-black.svg', // Replace with the path to your SVG icon
-//     iconSize: [80, 80], // Set the icon size
-// });
+//Define a custom icon with the desired color
+var redIcon = L.icon({
+    iconUrl: '../assets/images/location-icon-darkred.svg', // Replace with the path to your SVG icon
+    iconSize: [40, 40], // Set the icon size
+    iconAnchor:   [25, 40], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-9, -30] // point from which the popup should open relative to the iconAnchor
+})
 
-// //Add a marker to Cairo, Egypt with the custom icon
-// var marker = L.marker([30.0444, 31.2357], { icon: customIcon }).addTo(map);
+//Add a marker to Cairo, Egypt with the custom icon
+var marker = L.marker([30.0444, 31.2357], {icon: redIcon}).addTo(map);
 
-// Add a popup to the marker
+
+//// Add a popup to the marker
 marker.bindPopup("<b>Welcome to Cairo, Egypt!</b><br>This is a beautiful city.").openPopup();
 
 

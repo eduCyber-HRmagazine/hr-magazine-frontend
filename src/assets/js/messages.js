2b67818da987b1e8a839e6320347f94d7e4101b9
AyaKaseem <ayakaseem8@gmail.com>
2024-04-21 15:52:19 +0200

    updates

---
+++ src/assets/js/messages.js
@@ -0,0 +1,8 @@
+function toggleReplyBox() {
+    var replyBox = document.getElementById('replyBox');
+    if (replyBox.style.display === 'none') {
+        replyBox.style.display = 'block';
+    } else {
+        replyBox.style.display = 'none';
+    }
+}
